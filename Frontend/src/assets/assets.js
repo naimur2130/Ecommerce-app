49236212893448a37669165f4a1858dc331ffab1
Naimur Rashid Bhuiyan <naimurrasid@gmail.com>
2026-08-10 03:43:06 +0600

    First Commit for the Ecommerce MERN Project

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,17 @@
+import logo from "./logo.png";
+import search from "./search.png";
+import profile from "./profile.png";
+import cart from "./cart.png";
+import menu from "./menu.png";
+import dropdown from "./dropdown.png";
+import hero from "./hero.png";
+
+export const assets = {
+  logo,
+  search,
+  profile,
+  cart,
+  menu,
+  dropdown,
+  hero,
+};
