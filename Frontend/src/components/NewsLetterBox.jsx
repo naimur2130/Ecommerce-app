7e2df7e6f01731f4f5beb178c03d4c169c564549
Naimur Rashid Bhuiyan <naimurrasid@gmail.com>
2026-08-11 21:44:58 +0600

    Added subscription section

---
+++ Frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const NewsLetterBox = () => {
+  const onSubmitHandler = (event) => {
+    event.preventDefault(); //when we submit this form , it will not reload the webpage
+  };
+
+  return (
+    <div className="text-center">
+      <p text-2xl font-medium text-gray-800>
+        Subscribe now & get 20% off
+      </p>
+      <p className="text-gray-400 mt-3">
+        There are many variations of passages of Lorem Ipsum available, but the
+        majority have suffered alteration in some form
+      </p>
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3"
+      >
+        <input
+          className="w-full sm:flex-1 outline-none"
+          type="email"
+          placeholder="Enter your email"
+          required
+        />
+        <button
+          type="submit"
+          className="bg-black text-white text-xs px-10 py-4"
+        >
+          SUBSCRIBE
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewsLetterBox;
