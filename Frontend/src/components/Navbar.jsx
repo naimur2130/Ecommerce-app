23b00be279fed4835270002442fdc8384f090048
Naimur Rashid Bhuiyan <naimurrasid@gmail.com>
2026-08-12 18:23:50 +0600

    Login and Sign up page completed

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -42,7 +42,9 @@ const Navbar = () => {
           alt=""
         />
         <div className="group relative">
-          <img src={assets.profile} className="w-7 curson-pointer" alt="" />
+          <Link to={"/login"}>
+            <img src={assets.profile} className="w-7 curson-pointer" alt="" />
+          </Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="curson-pointer hover:text-black">My Profile</p>
