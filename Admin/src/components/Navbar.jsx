5eccd075b68c70dc5194ccd6a90bf0e5919c8e00
Naimur Rashid Bhuiyan <naimurrasid@gmail.com>
2026-08-14 03:00:21 +0600

    Admin login functionality added with backend api connection

--- Admin/src/components/Navbar.jsx
+++ Admin/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { assets } from "../assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
       <img
@@ -9,7 +9,10 @@ const Navbar = () => {
         src={assets.admin_logo}
         alt=""
       />
-      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full sm:text-sm">
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full sm:text-sm"
+      >
         Logout
       </button>
     </div>
