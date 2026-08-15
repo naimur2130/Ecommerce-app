c7cc7715ca1339304d196371ad159be3cb8812b0
Naimur Rashid Bhuiyan <naimurrasid@gmail.com>
2026-08-15 23:33:07 +0600

    Add Product page static UI ready

--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -2,10 +2,12 @@ import admin_logo from "./admin_logo.png";
 import add from "./add.png";
 import order from "./order.png";
 import list from "./list.png";
+import upload_image from "./upload_image.png";
 
 export const assets = {
   admin_logo,
   add,
   list,
   order,
+  upload_image,
 };
