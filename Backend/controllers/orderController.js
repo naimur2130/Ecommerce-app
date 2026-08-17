9ce4d44be6680b8ebb3f5d941384d407b6385fc4
Naimur Rashid Bhuiyan <naimurrasid@gmail.com>
2026-08-17 20:03:10 +0600

    Stripe payment gateway added

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,5 +1,12 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+
+//global vaiables
+const currency = "usd";
+const deliveryCharge = 100;
+//Payment Gateway Initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //Placing order using Cash on delivery method
 const placeOrder = async (req, res) => {
@@ -27,7 +34,72 @@ const placeOrder = async (req, res) => {
 };
 
 //Placing order using Stripe Method
-const placeOrderStripe = async (req, res) => {};
+const placeOrderStripe = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+    const { origin } = req.headers;
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now(),
+    };
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price,
+      },
+      quantity: item.quantity,
+    }));
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: "Delivery Fee",
+        },
+        unit_amount: deliveryCharge,
+      },
+      quantity: 1,
+    });
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+      line_items,
+      mode: "payment",
+    });
+
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const verifyStripe = async (req, res) => {
+  const { orderId, success, userId } = req.body;
+  try {
+    if (success) {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      await userModel.findByIdAndUpdate(userId, { cartData: {} });
+      res.json({ success: true });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //placing order using razor pay method
 
@@ -76,4 +148,5 @@ export {
   displayAllOrders,
   userOrders,
   updateStatus,
+  verifyStripe,
 };
--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -5,6 +5,7 @@ import {
   placeOrderStripe,
   displayAllOrders,
   userOrders,
+  verifyStripe,
   updateStatus,
 } from "../controllers/orderController.js";
 import adminAuth from "../middleware/adminAuth.js";
@@ -19,5 +20,7 @@ orderRouter.post("/place", auth, placeOrder);
 orderRouter.post("/stripe", auth, placeOrderStripe);
 orderRouter.post("/razorpay", auth, placeOrderRazorPay);
 orderRouter.post("/userorders", auth, userOrders);
+
+orderRouter.post("/verifystripe", auth, verifyStripe);
 
 export default orderRouter;
